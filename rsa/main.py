27a7978d799c44f17569b98300703395b9a219a1
Bulat F <write2bulat@gmail.com>
2017-10-13 23:36:40 +0300

    Implement generating keys

--- rsa/main.py
+++ rsa/main.py
@@ -1,3 +1,6 @@
+from random import randrange
+from random import getrandbits, randint
+
 def gcd(a, b):
     if a == 0:
         return [b, 0, 1]
@@ -5,17 +8,55 @@ def gcd(a, b):
     [d, x1, y1] = gcd(b % a, a)
     return [d, y1 - (b // a) * x1, x1]
 
+def check(a, s, d, n):
+    x = pow(a, d, n)
+    if x == 1:
+        return True
+    for i in range(s - 1):
+        if x == n - 1:
+            return True
+        x = pow(x, 2, n)
+    return x == n - 1
+
+def miller_rabin(n, k=10):
+    s = 0
+    d = n - 1
+
+    while d % 2 == 0:
+        d >>= 1
+        s += 1
+
+    for i in range(k):
+        a = randrange(2, n - 1)
+        if not check(a, s, d, n):
+            return False
+
+    return True
+
+def prime(bits = None, phi = None):
+    while True:
+        p = getrandbits(bits) if bits else randint(3, phi - 1)
+        if miller_rabin(p):
+            return p
+
 class Agent:
-    def __init__(self, p, q, selfE, foreignN, foreignE):
+    def __init__(self):
+        p = prime(bits = 16)
+        q = prime(bits = 16)
         phi = (p - 1)*(q - 1)
-        [__dontNeed1, __dontNeed2, selfD] = gcd(phi, selfE)
 
         self.selfN = p * q
-        self.selfE = selfE
-        self.selfD = selfD  if selfD > 0 else phi + selfD
+        self.selfE = prime(phi = phi)
 
-        self.foreignN = foreignN
-        self.foreignE = foreignE
+        [__dontNeed1, __dontNeed2, selfD] = gcd(phi, self.selfE)
+
+        self.selfD = selfD if selfD > 0 else phi + selfD
+
+    def getPublicKey(self):
+        return [self.selfE, self.selfN]
+
+    def setForeignKey(self, publicKey):
+        [self.foreignE, self.foreignN] = publicKey
 
     def encrypt(self, m):
         c = pow(m, self.selfD, self.selfN)
@@ -26,24 +67,15 @@ class Agent:
         return pow(m, self.foreignE, self.foreignN)
 
 if __name__ == '__main__':
+    alice = Agent()
+    bob = Agent()
 
-
-    # Alice
-    p1 = 46819
-    q1 = 65267
-    e1 = 913395991
-
-    # Bob
-    p2 = 53161
-    q2 = 50867
-    e2 = 764752679
+    alice.setForeignKey(bob.getPublicKey())
+    bob.setForeignKey(alice.getPublicKey())
 
     m = 111
-
-    alice = Agent(p1, q1, e1, p2 * q2, e2)
-    bob = Agent(p2, q2, e2, p1 * q1, e1)
-
     c = alice.encrypt(m)
+
     print(m)
     print(c)
     print(bob.decrypt(c))
